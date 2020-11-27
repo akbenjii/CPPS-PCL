67d6e1ce38d6f7824eabbb6c9bc0b7221d15b40d
akbenjii <73396619+akbenjii@users.noreply.github.com>
2020-11-27 09:54:06 -0500

    fix cpr support... again.

--- client.py
+++ client.py
@@ -221,7 +221,7 @@ class Client(object):
 		if kwargs.get("internal_room_id", True):
 			args.insert(0, self._internal_room_id)
 		if self._login_host == "server.cprewritten.net":
-			args.insert(0, (self._packet_code_num + 669567 ) ^ 842215) # thanks allinol for finding the function in the only file i didnt search :(
+			args.insert(0, (self._packet_code_num + 529887 ) ^ 924175) # cpr is gay 
 			self._packet_code_num += 1
 		packet += "".join(str(arg) + "%" for arg in args)
 		self._send(packet)
